9a8938606b78ac75e1448de2ac75b305f2b84fd7
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2018-01-07 14:41:18 +0200

    added sys arguments. printing number of steps. initialisation based on user defined screen size

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -3,7 +3,7 @@
 import time
 import curses
 from math import log10, ceil
-
+import sys
 
 class Item():
     x = None
@@ -22,9 +22,39 @@ class Item():
     def report_coordinate(self):
         return self.x, self.y
 
-x_size = 20
-y_size = 30
-steps = 100
+
+
+
+
+# initializing the system
+if "--help" in sys.argv[1:]:
+    print "welcome to the terminal based Game of Life!"
+    print "----------------------------"
+    print "here are the program arguments:"
+    print "\t\033[93m\033[1m-steps\033[0m           sets the number of steps that the program will run!"
+    print "\t\033[93m\033[1m-x\033[0m               sets the horizontal size of the screen"
+    print "\t\033[93m\033[1m-y\033[0m               sets the vertical size of the screen"
+    print "\t\033[93m\033[1m-glider\033[0m          initializes the glider"
+    print "\t\033[93m\033[1m-exploder\033[0m        initializes the exploder"
+    print "\t\033[93m\033[1m-small_exploder\033[0m  initializes the small_exploder"
+    sys.exit()
+if "-steps" in sys.argv[1:]:
+    steps = int(sys.argv[sys.argv.index("-steps")+1])
+else:
+    steps = 100
+
+if "-x" in sys.argv[1:]:
+    x_size = int(sys.argv[sys.argv.index("-x")+1])
+else:
+    x_size = 30
+
+if "-y" in sys.argv[1:]:
+    y_size = int(sys.argv[sys.argv.index("-y")+1])
+else:
+    y_size = 20
+
+center_x = int(ceil(x_size/2))
+center_y = int(ceil(y_size/2))
 
 dict_of_items = {}
 
@@ -34,43 +64,39 @@ for i in range(0, x_size):
         dict_of_items[i + j * x_size] = Item(i, j, False)
 
 
+if "-glider" in sys.argv[1:]:
+    # glider
+    dict_of_items[center_x+ (center_y+2)*x_size].life = True 
+    dict_of_items[center_x+ (center_y+1)*x_size].life = True 
+    dict_of_items[center_x+ (center_y+1)*x_size+2].life = True 
+    dict_of_items[center_x+ center_y*x_size].life = True 
+    dict_of_items[center_x+ center_y*x_size+1].life = True 
 
-# initializing the system
+elif "-exploder" in sys.argv[1:]:
+    # Exploder
+    dict_of_items[center_x+ (center_y+1)*x_size-4].life = True 
+    dict_of_items[center_x+ center_y*x_size-4].life = True 
+    dict_of_items[center_x+ center_y*x_size].life = True 
+    dict_of_items[center_x+ (center_y+1)*x_size].life = True 
+    dict_of_items[center_x+ (center_y+2)*x_size].life = True 
+    dict_of_items[center_x+ (center_y+2)*x_size-2].life = True 
+    dict_of_items[center_x+ (center_y+2)*x_size-4].life = True 
+    dict_of_items[center_x+ (center_y-1)*x_size].life = True 
+    dict_of_items[center_x+ (center_y-1)*x_size-4].life = True 
+    dict_of_items[center_x+ (center_y-2)*x_size].life = True 
+    dict_of_items[center_x+ (center_y-2)*x_size-2].life = True 
+    dict_of_items[center_x+ (center_y-2)*x_size-4].life = True 
 
-# glider
-"""
-dict_of_items[397].life = True
-dict_of_items[377].life = True
-dict_of_items[357].life = True
-dict_of_items[358].life = True
-dict_of_items[379].life = True
-"""
+elif "-small_exploder" in sys.argv[1:]:
+    # small exploder
+    dict_of_items[center_x+(center_y+1)*x_size].life = True
+    dict_of_items[center_x+(center_y+1)*x_size+1].life = True
+    dict_of_items[center_x+(center_y+1)*x_size+2].life = True
+    dict_of_items[center_x+(center_y+2)*x_size+1].life = True
+    dict_of_items[center_x+ center_y*x_size].life = True
+    dict_of_items[center_x+ center_y*x_size+2].life = True
+    dict_of_items[center_x+(center_y-1)*x_size+1].life = True
 
-
-# Exploder
-dict_of_items[308].life = True
-dict_of_items[310].life = True
-dict_of_items[312].life = True
-dict_of_items[288].life = True
-dict_of_items[292].life = True
-dict_of_items[268].life = True
-dict_of_items[272].life = True
-dict_of_items[248].life = True
-dict_of_items[252].life = True
-dict_of_items[228].life = True
-dict_of_items[230].life = True
-dict_of_items[232].life = True
-
-"""
-# small exploder
-dict_of_items[290].life = True
-dict_of_items[291].life = True
-dict_of_items[292].life = True
-dict_of_items[311].life = True
-dict_of_items[270].life = True
-dict_of_items[272].life = True
-dict_of_items[251].life = True
-"""
 
 # running it
 stdscr = curses.initscr()
@@ -118,14 +144,14 @@ while (k < steps):
             next_dict[item] = True
 
     number_of_digits = int(ceil(log10(x_size)))+1 
-    for i in range(1, number_of_digits):
-        stdscr.addstr(number_of_digits-i, 0,"      ")
-        for k in range(0, x_size):
-            if len(str(k)) >= i:
-                stdscr.addstr(number_of_digits-i, k+6, str(k)[len(str(k))-i])
+    stdscr.addstr(0, 0,"step: "+'%5s' % str(k))
+    for m in range(1, number_of_digits):
+        stdscr.addstr(number_of_digits-m, 0,"      ")
+        for l in range(0, x_size):
+            if len(str(l)) >= m:
+                stdscr.addstr(number_of_digits-m, l+6, str(l)[len(str(l))-m])
             else:
-                stdscr.addstr(number_of_digits-i, k+6, "0")
-
+                stdscr.addstr(number_of_digits-m, l+6, "0")
 
     stdscr.addstr(number_of_digits, 0, "     |"+"-"*x_size+"|")
     string = ""
@@ -140,14 +166,14 @@ while (k < steps):
         string += "\n"
         stdscr.addstr(j+1+number_of_digits, x_size+6, "|")
     stdscr.addstr(y_size+number_of_digits, 0, "     |"+"-"*x_size+"|")
-    time.sleep(0.5)
+    time.sleep(0.1)
     stdscr.refresh()
 
     for item in dict_of_items.keys():
         dict_of_items[item].update_life(next_dict[item])
     k += 1
     next_dict = {}
-
+    
 
 curses.echo()
 curses.nocbreak()
